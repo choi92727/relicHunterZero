8ed251c05551a82bd5efd0fa5abc42ccd7de850a
ans7578 <mgh0418@gmail.com>
2017-09-02 17:55:26 +0900

    임시 작업내용 커밋

---
+++ 170817_frameWork2D/gunInterface.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "gunInterface.h"
+
+
+gunInterface::gunInterface()
+{
+}
+
+
+gunInterface::~gunInterface()
+{
+}
+
+void gunInterface::fire()
+{
+}
+
+void gunInterface::setAngle()
+{
+}
---
+++ 170817_frameWork2D/gunInterface.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "gameNode.h"
+class gunInterface : public gameNode
+{
+protected:
+	float m_damage;
+	float m_speed;
+	float m_angle;
+	float m_x;
+	float m_y;
+	float m_width;
+	float m_height;
+	float m_range;
+public:
+	gunInterface();
+	~gunInterface();
+	virtual void fire();
+	virtual void setAngle();
+};
+
